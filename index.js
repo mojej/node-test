9e63ada86ac5bf1966ce35543ce1387b2dc44242
mojej <103126943+mojej@users.noreply.github.com>
2023-05-05 17:16:54 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+const port = 3000;
+// app.use(cors());
+
+app.get('/', (req, res) => {
+   res.send('Hello World!');
+});
+
+app.get('/sound/:name', (req, res, next) => {
+    const { name } = req.params;
+    if(name == 'dog') {
+        res.send({'sound':'멍멍'});
+    } else if(name == 'cat') {
+        res.send({'sound':'야옹'});
+    }  else if(name == 'pig') {
+        res.send({'sound':'꿀꿀'});
+    } else {
+        res.send({'sound':'알 수 없음'});
+    }
+    // res.json({});
+});
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`);
+});
